5edeec603515ac16614c078a0794c7cb5ebc1364
MohdFaisal21 <faisalmohd1357@gmail.com>
2026-01-01 13:45:20 +0530

    we've added city services

---
+++ src/services/city-service.js
@@ -0,0 +1,47 @@
+const {CityRepository}  = require("../repository");
+
+class CityService {
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (err) {
+            console.log("Something went wrong in the repository layer");
+            throw {err};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await this.cityRepository.updateCity(cityId, data);
+            return city;
+        } catch (err) {
+            console.log("Something went wrong in the repository layer");
+            throw {err};
+        }
+    }
+
+    async deleteCity(cityId){
+        try {
+            const response = this.cityRepository.deleteCity(cityId);
+            return response;
+        } catch (err) {
+            console.log("Something went wrong in the repository layer");
+            throw {err};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (err) {
+            console.log("Something went wrong in the repository layer");
+            throw {err};
+        }
+    }
+}
